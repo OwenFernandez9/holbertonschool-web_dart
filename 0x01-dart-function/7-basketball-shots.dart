d22b4dd7b807d52ee2363cd469074e08f4300ea7
OwenFernandez9 <9357@holbertonstudents.com>
2025-12-10 14:50:31 -0300

    7-basketball-shots

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,13 @@
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int scoreA = teamA['Free throws']! * 1 +
+               teamA['2 pointers']! * 2 +
+               teamA['3 pointers']! * 3;
+
+  int scoreB = teamB['Free throws']! * 1 +
+               teamB['2 pointers']! * 2 +
+               teamB['3 pointers']! * 3;
+
+  if (scoreA > scoreB) return 1;
+  if (scoreA < scoreB) return 2;
+  return 0;
+}
